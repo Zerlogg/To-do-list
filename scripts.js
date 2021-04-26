778ef83e635ae331ad653ecf088f4045fc634335
Emils Strelis <emilstrelis@inbox.lv>
2021-04-26 17:36:25 +0300

    Izmainas 5

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-document.getElementById("add-to-do").addEventListener("click", () => {
-  console.log("Poga nospiesta");
-  
-  let popup = document.getElementById("popup");
+document.getElementById('jauns_to-do').addEventListener('click', () => {
+  console.log("Poga nospiesta!");
 
-  popup.style.display = "block"
+  let popup = document.getElementById('popup');
+
+  popup.style.display = 'block'
 
 })
