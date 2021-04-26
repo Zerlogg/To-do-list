18553b14474b11bdf6337b587887d0a09f0132ea
Emils Strelis <emilstrelis@inbox.lv>
2021-04-26 21:10:31 +0300

    Izmainas 9

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 const POP_UP = document.getElementById('popup');
-let uzdevumi2 = []
+let uzdevumi2 = [];
+
+window.addEventListener('load', () => {
+  uzdevumi2 = JSON.parse(localStorage.getItem("uzdevumi2") || "[]");
+  console.log(uzdevumi2)
+  render();
+});
 
 document.getElementById('jaunsUzdevums').addEventListener('click', () => {
   POP_UP.style.display = 'block'
@@ -19,19 +25,18 @@ document.getElementById('pievienotUzdevumu').addEventListener('click', () => {
   render();
 })
 
-
 function render() {
   let uzdevumi3 = document.getElementById('uzdevumi3');
   uzdevumi3.innerHTML = "";
 
   for(let i = 0; i < uzdevumi2.length; i++) {
     let uzdevumi = `
-    <div class="uzdevumi2"> 
-      <h3>Uzdevums: ${uzdevumi2[i].uzdevums}</h4>
+    <div class="uzdevumi"> 
+      <h3>Uzdevums: ${uzdevumi2[i].uzdevums}</h3>
       <h4>Termiņš: ${uzdevumi2[i].termiņš}</h4> 
     </div>`;
   uzdevumi3.innerHTML += uzdevumi;
   }
 
-  
+  localStorage.setItem("uzdevumi2", JSON.stringify(uzdevumi2))
 }
