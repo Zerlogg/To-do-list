c7a75629664cf21ef510e2d50a6a4fdda249637f
Emils Strelis <emilstrelis@inbox.lv>
2021-04-26 18:54:33 +0300

    Izmainas 7

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
+const POP_UP = document.getElementById('popup');
+let uzdevumi2 = []
+
 document.getElementById('jaunsUzdevums').addEventListener('click', () => {
-  console.log("Poga nospiesta!");
-
-  let popup = document.getElementById('popup');
-
-  popup.style.display = 'block'
+  POP_UP.style.display = 'block'
 
 })
+
+document.getElementById('pievienotUzdevumu').addEventListener('click', () => {
+  POP_UP.style.display = 'none'
+
+  let uzdevumi = {uzdevums: uzdevums.value, termiņš: termiņš.value};
+  
+  uzdevums.value = ""
+  termiņš.value = ""
+
+  uzdevumi2.push(uzdevumi)
+})
+
+
+function render() {
+  let uzdevumi3 = document.getElementById('uzdevumi');
+  
+}
